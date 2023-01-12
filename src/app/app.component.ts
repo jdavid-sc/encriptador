8caf992cfd494dacca115c4f774701a8171d66f2
David Saldarriaga <102173604+jdavid-sc@users.noreply.github.com>
2023-01-11 23:10:53 -0500

    Encriptador con angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,52 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'encriptador';
+
+  texto:string = '';
+  textoListo: string = '';
+
+
+  claves =  [
+    {
+      letra: 'a',
+      clave: 'ai'
+    },
+    {
+      letra: 'e',
+      clave: 'enter'
+    },
+    {
+      letra: 'i',
+      clave: 'imes'
+    },
+    {
+      letra: 'o',
+      clave: 'ober'
+    },
+    {
+      letra: 'u',
+      clave: 'ufat'
+    },
+  ]
+
+  encriptar(){
+      const textoDesencriptar = this.texto.split('')
+    textoDesencriptar.forEach((keyLetra, index) => {
+      const letraEncontrada =  this.claves.find(key => key.letra == keyLetra )
+      if(letraEncontrada){
+        textoDesencriptar[index] = letraEncontrada.clave
+         this.textoListo = textoDesencriptar.join('')
+      }
+
+    })
+  }
+
+  desencriptar(){
+    this.textoListo = this.texto
+  }
+
+  copiarTexto(){  
+    
+
+  }
 }
